6129d8fcf860ccfdad8f69b9135fd02ca1753028
marioadiaz <mario.a.diaz@live.com.ar>
2021-05-05 11:47:27 -0300

    se eliminan formularios y funcionesque no se usan, queda pendiente la asigancion automatica de fechas de proximo tratamiento

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,28 +2,20 @@ class Cliente < ApplicationRecord
 	has_many :orden_fumigacions
 	has_many :tecnicos, through: :orden_fumigacions
 
+	validates :nombre, presence: true
+	validates :apellido, presence: true
 
-validates :nombre, presence: true
-validates :apellido, presence: true
+	before_create { |cliente| cliente.apellido = cliente.apellido.upcase }
+	before_create { |cliente| cliente.nombre = cliente.nombre.upcase }
+	before_create { |cliente| cliente.rubro = cliente.rubro.upcase }
+	before_create { |cliente| cliente.domicilio = cliente.domicilio.upcase }
 
-
-
-before_create { |cliente| cliente.apellido = cliente.apellido.upcase }
-before_create { |cliente| cliente.nombre = cliente.nombre.upcase }
-before_create { |cliente| cliente.rubro = cliente.rubro.upcase }
-before_create { |cliente| cliente.domicilio = cliente.domicilio.upcase }
-
-
-before_update { |cliente| cliente.apellido = cliente.apellido.upcase }
-before_update { |cliente| cliente.nombre = cliente.nombre.upcase }
-before_update{ |cliente| cliente.rubro = cliente.rubro.upcase }
-before_update { |cliente| cliente.domicilio = cliente.domicilio.upcase }
-
-
-
+	before_update { |cliente| cliente.apellido = cliente.apellido.upcase }
+	before_update { |cliente| cliente.nombre = cliente.nombre.upcase }
+	before_update{ |cliente| cliente.rubro = cliente.rubro.upcase }
+	before_update { |cliente| cliente.domicilio = cliente.domicilio.upcase }
 
     TIPO_CLIENTE = ['COMERCIAL', 'INDUSTRIAL', 'PARTICULAR', 'EDUCACIONAL', 'ESTATAL', 'OTROS']
-
 
     def self.buscador(termino)
       Cliente.where("apellido LIKE ?", "%#{termino}%")
