f712d7173e084b194c145ff2260190eddb64c944
marioadiaz <mario.a.diaz@live.com.ar>
2021-05-10 11:14:53 -0300

    MODIFICACION EN EL REPORTE DE FAJA

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # configuracion devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #config.hosts = ["sisa-md.duckdns.org"]
+  config.hosts = ["localhost"]
 end
