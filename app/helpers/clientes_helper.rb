7257dca2f788cbb53d36d36933cc41d7fe139622
marioadiaz <mario.a.diaz@live.com.ar>
2022-01-12 22:02:58 -0300

    AGREGAR CAMPOS REFUERZO Y REPASO

--- app/helpers/clientes_helper.rb
+++ app/helpers/clientes_helper.rb
@@ -1,16 +1,16 @@
 module ClientesHelper
 
     def procesarelemento(elemento)
-    	vectorelemento = elemento.split(" ")
-    	resultado = nil
-    	vectorelemento.each do |valor|
-    		if resultado.nil?
-    			resultado = valor
-    		else
-    			resultado = resultado + "% %" + valor
-    		end
-    	end
-    	return resultado
+        vectorelemento = elemento.split(" ")
+        resultado = nil
+        vectorelemento.each do |valor|
+            if resultado.nil?
+                resultado = valor
+            else
+                resultado = resultado + "% %" + valor
+            end
+        end
+        return resultado
     end
 
 end
